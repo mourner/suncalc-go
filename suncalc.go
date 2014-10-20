f42427f4f1fdeb3b866f1544ae0a7755ef432f0b
Vladimir Agafonkin <agafonkin@gmail.com>
2014-10-20 20:33:31 +0300

    prettify

--- suncalc.go
+++ suncalc.go
@@ -2,17 +2,19 @@ package main
 
 import (
 	"fmt"
-	"math"
+	m "math"
 	"time"
 )
 
-const rad = math.Pi / 180
+const rad = m.Pi / 180
 
 // time conversions
 
-const daySec = 60 * 60 * 24
-const j1970 = 2440588.0
-const j2000 = 2451545.0
+const (
+	daySec = 60 * 60 * 24
+	j1970 = 2440588.0
+	j2000 = 2451545.0
+)
 
 func toJulian(t time.Time) float64 {
 	return float64(t.Unix()) / daySec - 0.5 + j1970
@@ -29,16 +31,16 @@ func toDays(t time.Time) float64 {
 const e = rad * 23.4397
 
 func rightAscension(l, b float64) float64 {
-	return math.Atan2(math.Sin(l) * math.Cos(e) - math.Tan(b) * math.Sin(e), math.Cos(l))
+	return m.Atan2(m.Sin(l) * m.Cos(e) - m.Tan(b) * m.Sin(e), m.Cos(l))
 }
 func declination(l, b float64) float64 {
-	return math.Asin(math.Sin(b) * math.Cos(e) + math.Cos(b) * math.Sin(e) * math.Sin(l))
+	return m.Asin(m.Sin(b) * m.Cos(e) + m.Cos(b) * m.Sin(e) * m.Sin(l))
 }
 func azimuth(H, phi, dec float64) float64 {
-	return math.Atan2(math.Sin(H), math.Cos(H) * math.Sin(phi) - math.Tan(dec) * math.Cos(phi))
+	return m.Atan2(m.Sin(H), m.Cos(H) * m.Sin(phi) - m.Tan(dec) * m.Cos(phi))
 }
 func altitude(H, phi, dec float64) float64 {
-	return math.Sin(math.Sin(phi) * math.Sin(dec) + math.Cos(phi) * math.Cos(dec) * math.Cos(H))
+	return m.Sin(m.Sin(phi) * m.Sin(dec) + m.Cos(phi) * m.Cos(dec) * m.Cos(H))
 }
 func siderealTime(d, lw float64) float64 {
 	return rad * (280.16 + 360.9856235 * d) - lw
@@ -49,10 +51,10 @@ func siderealTime(d, lw float64) float64 {
 func solarMeanAnomaly(d float64) float64 {
 	return rad * (357.5291 + 0.98560028 * d)
 }
-func eclipticLongitude(m float64) float64 {
-	c := rad * (1.9148 * math.Sin(m) + 0.02 * math.Sin(2 * m) + 0.0003 * math.Sin(3 * m)) // equation of center
+func eclipticLongitude(ma float64) float64 {
+	c := rad * (1.9148 * m.Sin(ma) + 0.02 * m.Sin(2 * ma) + 0.0003 * m.Sin(3 * ma)) // equation of center
 	p := rad * 102.9372 // perihelion of the Earth
-	return m + c + p + math.Pi
+	return ma + c + p + m.Pi
 }
 func sunCoords(d float64) (float64, float64) {
 	l := eclipticLongitude(solarMeanAnomaly(d))
@@ -76,16 +78,16 @@ func SunPosition(t time.Time, lat, lng float64) (float64, float64) {
 const j0 = 0.0009
 
 func julianCycle(d, lw float64) float64 {
-	return math.Floor(d - j0 - lw / (2.0 * math.Pi) + 0.5)
+	return m.Floor(d - j0 - lw / (2.0 * m.Pi) + 0.5)
 }
 func approxTransit(ht, lw, n float64) float64 {
-	return j0 + (ht + lw) / (2.0 * math.Pi) + n
+	return j0 + (ht + lw) / (2.0 * m.Pi) + n
 }
-func solarTransitJ(ds, m, l float64) float64 {
-	return j2000 + ds + 0.0053 * math.Sin(m) - 0.0069 * math.Sin(2 * l)
+func solarTransitJ(ds, ma, l float64) float64 {
+	return j2000 + ds + 0.0053 * m.Sin(ma) - 0.0069 * m.Sin(2 * l)
 }
 func hourAngle(h, phi, d float64) float64 {
-	return math.Acos((math.Sin(h) - math.Sin(phi) * math.Sin(d)) / (math.Cos(phi) * math.Cos(d)))
+	return m.Acos((m.Sin(h) - m.Sin(phi) * m.Sin(d)) / (m.Cos(phi) * m.Cos(d)))
 }
 
 // returns set time for the given sun altitude
@@ -103,7 +105,7 @@ type SunAngle struct {
 	setName  string
 }
 
-var sunAngles []SunAngle = []SunAngle{
+var sunAngles = [...]SunAngle{
 	SunAngle{-0.833, "sunrise", "sunset"},
 	SunAngle{-0.3, "sunriseEnd", "sunsetStart"},
 	SunAngle{-6.0, "dawn", "dusk"},
@@ -121,11 +123,11 @@ func SunTimes(t time.Time, lat, lng float64) map[string]time.Time {
 	n := julianCycle(d, lw)
 	ds := approxTransit(0, lw, n)
 
-	m := solarMeanAnomaly(ds)
-	l := eclipticLongitude(m)
+	ma := solarMeanAnomaly(ds)
+	l := eclipticLongitude(ma)
 	dec := declination(l, 0)
 
-	jNoon := solarTransitJ(ds, m, l)
+	jNoon := solarTransitJ(ds, ma, l)
 
 	times := map[string]time.Time{
 		"solarNoon": fromJulian(jNoon),
@@ -133,7 +135,7 @@ func SunTimes(t time.Time, lat, lng float64) map[string]time.Time {
 	}
 
 	for _, sunAngle := range sunAngles {
-		jSet := getSetJ(sunAngle.angle * rad, lw, phi, dec, n, m, l)
+		jSet := getSetJ(sunAngle.angle * rad, lw, phi, dec, n, ma, l)
 
 		times[sunAngle.riseName] = fromJulian(jNoon - (jSet - jNoon))
 		times[sunAngle.setName] = fromJulian(jSet)
